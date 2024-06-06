50f78504715ad63c3f504353a89a5968669b3b00
RufatRahmatov <rehmetovruft@gmail.com>
2024-06-06 03:40:08 -0700

    Kod yenilendi

--- js/app.js
+++ js/app.js
@@ -1,39 +1,40 @@
+
 function updateColor(){
-    const card=document.querySelectorAll(card__cards);
-    card.forEach((card)=>{
-        const logo = card.querySelector("card__logo");
-        const btn = card.querySelector('btn__request');
+    const cards = document.querySelectorAll(".card__cards");
 
-        btn.style.color = "black";
-        logo.style.backgoundColor ="black";
+    cards.forEach((card) => {
+        const logo = card.querySelector(".card__logo");
+        const btn = card.querySelector('.btn__request');
 
-        btn.addEventListener("mouseover", () =>{
-            if(logo.classList.contains("request")){
-                setColor ( logo,btn, "#E2BE7C" )
-            } else if(logo.classList.contains("pending")) {
-                setColor(logo,btn,"#E2BE7C");
-            }else if (logo.classList.contains("done")){
-                setColor(logo,btn,"#E2BE7C");
-            }else if (logo.classList.contains("rich")){
-                setColor(logo,btn,"#E2BE7C");
-            }
-            btn.style.color ="white";
-        });
+        setInitialStyles(logo, btn);
 
-        btn.addEventListener("mouseout", ()=>{
-            setColor(logo,btn, "white");
-            logo.style.backgoundColor ="black";
-            btn.style.color ="black";
-        });
-
+        btn.addEventListener("mouseover", () => handleMouseOver(logo, btn));
+        btn.addEventListener("mouseout", () => handleMouseOut(logo, btn));
     });
-   
+}
+
+function setInitialStyles(logo, btn) {
+    btn.style.color = "black";
+    logo.style.backgroundColor = "black";
+}
+
+function handleMouseOver(logo, btn) {
+    if (logo.classList.contains("request") 
+        ); {
+        setColor(logo, btn, "#FF6725");
+    }
+    btn.style.color = "white";
+}
+
+function handleMouseOut(logo, btn) {
+    setColor(logo, btn, "white");
+    logo.style.backgroundColor = "black";
+    btn.style.color = "black";
 }
 
 function setColor(logo, btn, color) {
-  logo.style.backgroundColor = color;
-  btn.style.backgroundColor = color;
+    logo.style.backgroundColor = color;
+    btn.style.backgroundColor = color;
 }
 
 updateColor();
-
